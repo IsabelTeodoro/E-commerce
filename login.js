85c25e93f07896841675ad674ae59af3d182a030
IsabelTeodoro <isabelteovas@gmail.com>
2024-10-01 20:53:00 -0300

    novas atualizacoes

--- login.js
+++ login.js
@@ -1,9 +1,11 @@
 
+
 // Dados de usuário pré-definidos
 const predefinedUsers = [
     { username: "usuario1", password: "senha1" },
     { username: "usuario2", password: "senha2" }
 ];
+
 function handleLogin(event) {
     event.preventDefault(); // Impede o envio do formulário
 
@@ -13,11 +15,9 @@ function handleLogin(event) {
 
     // Verifica as credenciais
     const user = predefinedUsers.find(user => user.username === username && user.password === password);
-
     if (user) {
-        // Exibe a mensagem de boas-vindas e a pop-up
         document.getElementById("welcomeMessage").innerText = `Bem-vindo, ${user.username}! Você está logado.`;
-        document.getElementById("loginPopup").style.display = "block"; // Mostra a pop-up
+        document.getElementById("loginPopup").style.display = "flex"; // Altera para "flex" para garantir que o layout flexível funcione
         document.getElementById("logoutButton").style.display = "block"; // Mostra o botão de logout
         loginMessage.innerText = ''; // Limpa mensagens anteriores
     } else {
@@ -25,9 +25,32 @@ function handleLogin(event) {
         loginMessage.innerText = 'Usuário ou senha incorretos.';
     }
 }
+
 function closePopup() {
     document.getElementById("loginPopup").style.display = "none"; // Esconde a pop-up
 }
 
+// Função de logout
+function handleLogout() {
+    // Oculta o botão de logout
+    document.getElementById("logoutButton").style.display = "none";
+    
+    // Limpa a mensagem de boas-vindas
+    document.getElementById("welcomeMessage").innerText = '';
+    
+    // Esconde a pop-up de login
+    closePopup();
+
+    // Limpa os campos de entrada
+    document.getElementById("username").value = '';
+    document.getElementById("password").value = '';
+
+    // Mostra uma mensagem informando que o usuário saiu
+    const loginMessage = document.getElementById("loginMessage");
+    loginMessage.innerText = 'Você saiu com sucesso.';
+}
+
 // Adiciona um listener de evento ao botão de logout
 document.getElementById('logoutButton').addEventListener('click', handleLogout);
+
+
